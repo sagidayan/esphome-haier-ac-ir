6bf734c0ccc21a1a5a44a34c5ace3795a2af533e
Sagi Dayan <sagidayan@gmail.com>
2026-05-22 12:33:28 +0300

    Updated deprecated functions
    see https://github.com/esphome/esphome/pull/16289
    
    Signed-off-by: Sagi Dayan <sagidayan@gmail.com>

--- components/haier_acyrw02/haier_acyrw02.cpp
+++ components/haier_acyrw02/haier_acyrw02.cpp
@@ -96,7 +96,7 @@ climate::ClimateTraits HaierClimate::traits() {
   traits.set_visual_max_temperature(HAIER_ACYRW02_TEMP_MAX);
   traits.set_visual_min_temperature(HAIER_ACYRW02_TEMP_MIN);
   traits.set_visual_temperature_step(1);
-  traits.set_supports_current_temperature(true);
+  traits.add_feature_flags(climate::CLIMATE_SUPPORTS_CURRENT_TEMPERATURE);
 
   return traits;
 }
